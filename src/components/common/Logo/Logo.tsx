d023281d24f5568b9f538dfa3acb90a8fa588b7e
Irina <irene7744@gmail.com>
2022-06-02 21:13:39 +0300

    fix: change logo routing

--- src/components/common/Logo/Logo.tsx
+++ src/components/common/Logo/Logo.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import logoImg from '../../../assets/images/logo-icon.svg';
 import style from './Logo.module.scss';
 
 const Logo = () => (
-  <a className={style.logo} href="/">
+  <NavLink className={style.logo} to="/">
     <img className={style.logo__img} alt="logo " src={logoImg} />
     <h1 className={style.logo__text}>СТИКЕР</h1>
-  </a>
+  </NavLink>
 );
 
 export default Logo;
