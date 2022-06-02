63f0a636d70360b53af6fc416c47e68a0b0a7ad8
Irina <irene7744@gmail.com>
2022-06-02 21:59:59 +0300

    feat: add banner

--- src/components/Pages/MainPage/index.tsx
+++ src/components/Pages/MainPage/index.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import img from '../../../assets/images/main-page-banner-pic.png';
+import style from './MainPage.module.scss';
 
-const MainPage = () => <h1>BANNER</h1>;
+const MainPage = () => (
+  <div className={style.wrapper}>
+    <div className={style.info}>
+      <h2 className={style.title}>Доска объявлений</h2>
+      <p className={style.text}>
+        Находи тысячи разнообразных товаров и услуг от продавцов со всей страны. Безопасные расчеты.
+        Удобный сервис доставки
+      </p>
+    </div>
+    <img className={style.img} src={img} alt="Cart" />
+  </div>
+);
 
 export default MainPage;
