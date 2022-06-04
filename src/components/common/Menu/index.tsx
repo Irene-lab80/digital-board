74319e267a9c76ed6995092b068b4e74c345638e
Irina <irene7744@gmail.com>
2022-06-04 17:19:32 +0300

    feat: add menu styles

--- src/components/common/Menu/index.tsx
+++ src/components/common/Menu/index.tsx
@@ -2,9 +2,18 @@ import React from 'react';
 import style from './Menu.module.scss';
 
 const Menu = () => (
-  <div className={style.wrapper}>
-    <h2>MENU</h2>
-  </div>
+  <nav>
+    <ul className={style.menu}>
+      <li className={style.menuItem_accented}>Вся доска</li>
+      <li className={style.menuItem_bordered}>Автомобили</li>
+      <li className={style.menuItem}>Аксессуары</li>
+      <li className={style.menuItem}>Мебель</li>
+      <li className={style.menuItem}>Одежда</li>
+      <li className={style.menuItem}>Спорт</li>
+      <li className={style.menuItem}>Техника</li>
+      <li className={style.menuItem}>Товары для дома</li>
+    </ul>
+  </nav>
 );
 
 export default Menu;
