a5a61f831d6ba009c776b140bc80d321d28711a5
Irina <irene7744@gmail.com>
2022-06-08 18:36:25 +0300

    fix: remove rub from data

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -10,83 +10,92 @@ import img8 from '../assets/images/8.jpg';
 import img9 from '../assets/images/9.jpg';
 
 export const data = [
-  { id: 1,
+  {
+    id: 1,
     tag: 'Техника',
     title: 'Стиральная машина Bosch',
     description: 'Машина в хорошем состоянии. Возможен разумный торг',
-    price: '22 500 Р',
+    price: '22 500',
     date: '14 апреля 2021',
     viewNumber: '365',
     src: img1
   },
-  { id: 2,
+  {
+    id: 2,
     tag: 'Одежда',
     title: 'Шапочка',
     description: 'Шапочка детская на ребенка 7-10 лет. 60% шерст, 40% вискоза',
-    price: '500 Р',
+    price: '500',
     date: '14 апреля 2021',
     viewNumber: '365',
     src: img2
   },
-  { id: 3,
+  {
+    id: 3,
     tag: 'Одежда',
     title: 'Толстовка',
     description: 'Новая толстовка белая без принтас карманом',
-    price: '2 000 Р',
+    price: '2 000',
     date: '14 апреля 2021',
     viewNumber: '365',
     src: img3
   },
-  { id: 4,
+  {
+    id: 4,
     tag: 'Спорт',
     title: 'Велосипед',
     description: 'Велосипед в отличном состоянии. Ретро-стиль',
-    price: '28 000 Р',
+    price: '28 000',
     date: '14 апреля 2021',
     viewNumber: '365',
     src: img4
   },
-  { id: 5,
+  {
+    id: 5,
     tag: 'Техника',
     title: 'Наушники',
     description: 'Наушники почти новые. Срок эксплуатации - меньше месяца',
-    price: '4 500 Р',
+    price: '4 500',
     date: '14 апреля 2021',
     viewNumber: '365',
     src: img5
   },
-  { id: 6,
+  {
+    id: 6,
     tag: 'Техника',
     title: 'Кофемашина',
     description: 'Кофемашина с капучинатором. Пакет кофе 1 кг. - в подарок',
-    price: '8 500 Р',
+    price: '8 500',
     date: '14 апреля 2021',
     viewNumber: '365',
     src: img6
   },
-  { id: 7,
+  {
+    id: 7,
     tag: 'Одежда',
     title: 'Сумка женская',
     description: 'Итальянска сумка из натуральной кожи. Качественная фурнитура, состояние новой',
-    price: '7 500 Р',
+    price: '7 500',
     date: '14 апреля 2021',
     viewNumber: '365',
     src: img7
   },
-  { id: 8,
+  {
+    id: 8,
     tag: 'Одежда',
     title: 'Ботинки мужские зимние',
     description: 'Ботинки почти новые, качественные, теплые. Носили 2 недели',
-    price: '8 000 Р',
+    price: '8 000',
     date: '14 апреля 2021',
     viewNumber: '365',
     src: img8
   },
-  { id: 9,
+  {
+    id: 9,
     tag: 'Спорт',
     title: 'Доска для сноубординга',
     description: 'Качественная красивая доска с креплениями',
-    price: '43 000 Р',
+    price: '43 000',
     date: '14 апреля 2021',
     viewNumber: '365',
     src: img9
