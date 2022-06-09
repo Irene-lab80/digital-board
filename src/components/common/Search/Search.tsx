1a6c9a7123888a3c8e4543f2afed2aedfde96017
Irina <irene7744@gmail.com>
2022-06-09 20:01:49 +0300

    feat: remove redundant code  from search comp

--- src/components/common/Search/Search.tsx
+++ src/components/common/Search/Search.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 // eslint-disable-next-line import/no-unresolved
 import Search from 'antd/lib/input/Search';
 import { SearchOutlined } from '@ant-design/icons';
-import '../../../assets/styles/_constants.scss';
 
 const onSearch = (value: string) => console.log(value);
 
 const suffix = (
   <SearchOutlined
     style={{
-      color: '$color-text-dark-l',
       opacity: '0.4',
     }}
   />
@@ -17,7 +15,6 @@ const suffix = (
 
 export default () => (
   <Search
-    // style={{ minWidth: '565px' }}
     placeholder=""
     allowClear
     enterButton="Искать"
