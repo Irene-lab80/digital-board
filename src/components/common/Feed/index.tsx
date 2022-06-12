477e30ea005b806b3e46b49b903fa0a17b07d7a8
Irina <irene7744@gmail.com>
2022-06-12 16:57:00 +0300

    some fixes

--- src/components/common/Feed/index.tsx
+++ src/components/common/Feed/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Card from '../Card';
 import style from './Feed.module.scss';
 import { data } from '../../../helpers';
@@ -10,17 +9,16 @@ const Feed = () => (
     <h2>Вся лента</h2>
     <div className={style.cardsWrapper}>
       { data.map((el) => (
-        <Link to="product">
-          <Card
-            tag={el.tag}
-            title={el.title}
-            description={el.description}
-            price={el.price}
-            date={el.date}
-            viewNumber={el.viewNumber}
-            imgPath={el.src}
+        <Card
+          key={el.id}
+          tag={el.tag}
+          title={el.title}
+          description={el.description}
+          price={el.price}
+          date={el.date}
+          viewNumber={el.viewNumber}
+          imgPath={el.src}
           />
-        </Link>
       ))}
     </div>
     <div className={style.btnWrapper}>
