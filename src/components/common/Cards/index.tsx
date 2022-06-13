020c63309c34d304caded50b606f76bb2aa74967
Irina <irene7744@gmail.com>
2022-06-13 15:04:31 +0300

    feat: add media to cards

--- src/components/common/Cards/index.tsx
+++ src/components/common/Cards/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { useMediaQuery } from 'react-responsive';
 import Card from './Card';
 import { data } from '../../../helpers';
 
 // TODO: remove any
-const Cards: any = () => (
-  data.map((el) => (
+const Cards: any = () => {
+  const arrayOfCards = data.map((el) => (
     <Card
       key={el.id}
       tag={el.tag}
@@ -15,7 +16,49 @@ const Cards: any = () => (
       viewNumber={el.viewNumber}
       imgPath={el.src}
     />
-  ))
-);
+  ));
+
+  const arrayOfCardTablet = [];
+  const arrayOfCardMobile = [];
+
+  for (let i = 0; i < 6; i += 1) {
+    while (i < 6) {
+      arrayOfCardTablet.push(arrayOfCards[i]);
+      i += 1;
+    }
+  }
+
+  for (let i = 0; i < 3; i += 1) {
+    while (i <= 2) {
+      arrayOfCardMobile.push(arrayOfCards[i]);
+      i += 1;
+    }
+  }
+
+  type PropsType = {
+    children: any
+    // React.ReactNode
+  }
+
+  const Desktop: React.FC<PropsType> = ({ children }) => {
+    const isDesktop = useMediaQuery({ minWidth: 992 });
+    return isDesktop ? children : null;
+  };
+  const Tablet: React.FC<PropsType> = ({ children }) => {
+    const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 });
+    return isTablet ? children : null;
+  };
+  const Mobile: React.FC<PropsType> = ({ children }) => {
+    const isMobile = useMediaQuery({ maxWidth: 767 });
+    return isMobile ? children : null;
+  };
+  return (
+    <>
+      <Desktop>{arrayOfCards}</Desktop>
+      <Tablet>{arrayOfCardTablet}</Tablet>
+      <Mobile>{arrayOfCardMobile}</Mobile>
+    </>
+  );
+};
 
 export default Cards;
