caa007aabeb4cef45036cc56351cff647afbbfc3
Irina <irene7744@gmail.com>
2022-06-15 18:27:52 +0300

    feat: add loadSvg

--- src/components/common/Feed/index.tsx
+++ src/components/common/Feed/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import style from './Feed.module.scss';
 import CustomButton from '../CustomButton';
 import Cards from '../Cards';
+import LoadSvg from '../svg/LoadSvg';
 
 const Feed = () => (
   <div className={style.feedwrapper}>
@@ -11,8 +12,10 @@ const Feed = () => (
     </div>
     <div className={style.btnWrapper}>
       <CustomButton onClick={() => {}} buttonStyle="btn--outline">
-        <span className="icon--more" />
-        Загрузить еще
+        <LoadSvg />
+        <span className={style.btnText}>
+          Загрузить еще
+        </span>
       </CustomButton>
     </div>
   </div>
