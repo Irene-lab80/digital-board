d5780ff9da76c1afe3c01ff698c713d3e9a67078
Irina <irene7744@gmail.com>
2022-06-18 20:39:57 +0300

    deploy

--- src/components/Pages/AuthPage/index.tsx
+++ src/components/Pages/AuthPage/index.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
 import AuthForm from '../../common/AuthForm';
 import FormHeader from '../../common/FormHeader';
 
-const AuthPage = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  // const fromPage = location.state?.from?.pathname || '/';
-  return (
-    <>
-      <FormHeader />
-      <AuthForm />
-    </>
-  );
-};
-
+const AuthPage = () => (
+  <>
+    <FormHeader />
+    <AuthForm />
+  </>
+);
 export default AuthPage;
