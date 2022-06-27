fa1395b963177ddc79d548f234dd076800f81429
Irina <irene7744@gmail.com>
2022-06-27 20:41:28 +0300

    fix: add filterhandler buttons

--- src/components/common/Feed/index.tsx
+++ src/components/common/Feed/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import style from './Feed.module.scss';
 import CustomButton from '../CustomButton';
@@ -11,35 +11,25 @@ import getProducts from '../../../store/products/selectors';
 const Feed = () => {
   const productData = useSelector(getProducts);
 
-  // const [filter, setFilter]: [any, any] = useState('Все товары');
-  // const filterHandler = (event: any) => {
-  //   setFilter(event.target.getAttribute('id'));
-  // };
+  const [filter, setFilter]: [any, any] = useState('Все товары');
+  const filterHandler = (event: any) => {
+    setFilter(event.target.getAttribute('id'));
+  };
 
+  // @ts-ignore
+  const menuItems = [...new Set(productData.map((Val) => Val.tag))];
+  console.log(menuItems);
   return (
     <div className={style.feedwrapper}>
-      {/* <nav className={style.nav}>
-        <div className={style.menu}>
-          <MenuItem className={filter === 'Все товары' ? 'menu-item--active menu-item' : 'menu-item'} id="Все товары" onClick={filterHandler}>Все товары</MenuItem>
-          <MenuItem className={filter === 'Автомобили' ? 'menu-item--active  menu-item' : 'menu-item'} id="Автомобили" onClick={filterHandler}>Автомобили</MenuItem>
-          <MenuItem className={filter === 'Аксессуары' ? 'menu-item--active  menu-item' : 'menu-item'} id="Аксессуары" onClick={filterHandler}>Аксессуары</MenuItem>
-          <MenuItem className={filter === 'Мебель' ? 'menu-item--active  menu-item' : 'menu-item'} id="Мебель" onClick={filterHandler}>Мебель</MenuItem>
-          <MenuItem className={filter === 'Одежда' ? 'menu-item--active  menu-item' : 'menu-item'} id="Одежда" onClick={filterHandler}>Одежда</MenuItem>
-          <MenuItem className={filter === 'Спорт' ? 'menu-item--active  menu-item' : 'menu-item'} id="Спорт" onClick={filterHandler}>Спорт</MenuItem>
-          <MenuItem className={filter === 'Техника' ? 'menu-item--active  menu-item' : 'menu-item'} id="Техника" onClick={filterHandler}>Техника</MenuItem>
-          <MenuItem className={filter === 'Товары для дома' ? 'menu-item--active  menu-item' : 'menu-item'} id="Товары для дома" onClick={filterHandler}>Товары для дома</MenuItem>
-        </div>
-      </nav> */}
-
       <nav className={style.scrollingWrapper}>
-        <MenuItem className={style.menuItemActive} id="Все товары" onClick={() => {}}>Все товары</MenuItem>
-        <MenuItem className={style.menuItem} id="Автомобили" onClick={() => {}}>Автомобили</MenuItem>
-        <MenuItem className={style.menuItem} id="Аксессуары" onClick={() => {}}>Аксессуары</MenuItem>
-        <MenuItem className={style.menuItem} id="Мебель" onClick={() => {}}>Мебель</MenuItem>
-        <MenuItem className={style.menuItem} id="Одежда" onClick={() => {}}>Одежда</MenuItem>
-        <MenuItem className={style.menuItem} id="Спорт" onClick={() => {}}>Спорт</MenuItem>
-        <MenuItem className={style.menuItem} id="Техника" onClick={() => {}}>Техника</MenuItem>
-        <MenuItem className={style.menuItem} id="Товары для дома" onClick={() => {}}>Товары для дома</MenuItem>
+        <MenuItem className={filter === 'Все товары' ? 'menu-item--active menu-item' : 'menu-item'} id="Все товары" onClick={filterHandler}>Все товары</MenuItem>
+        <MenuItem className={filter === 'Автомобили' ? 'menu-item--active  menu-item' : 'menu-item'} id="Автомобили" onClick={filterHandler}>Автомобили</MenuItem>
+        <MenuItem className={filter === 'Аксессуары' ? 'menu-item--active  menu-item' : 'menu-item'} id="Аксессуары" onClick={filterHandler}>Аксессуары</MenuItem>
+        <MenuItem className={filter === 'Мебель' ? 'menu-item--active  menu-item' : 'menu-item'} id="Мебель" onClick={filterHandler}>Мебель</MenuItem>
+        <MenuItem className={filter === 'Одежда' ? 'menu-item--active  menu-item' : 'menu-item'} id="Одежда" onClick={filterHandler}>Одежда</MenuItem>
+        <MenuItem className={filter === 'Спорт' ? 'menu-item--active  menu-item' : 'menu-item'} id="Спорт" onClick={filterHandler}>Спорт</MenuItem>
+        <MenuItem className={filter === 'Техника' ? 'menu-item--active  menu-item' : 'menu-item'} id="Техника" onClick={filterHandler}>Техника</MenuItem>
+        <MenuItem className={filter === 'Товары для дома' ? 'menu-item--active  menu-item' : 'menu-item'} id="Товары для дома" onClick={filterHandler}>Товары для дома</MenuItem>
       </nav>
       <div className={style.titleWrapper}>
         <h2 className={style.title}>Вся лента</h2>
