581e8a7d765028b6accd994720142986a62a3a98
Irina <irene7744@gmail.com>
2022-06-27 22:10:06 +0300

    feat: add filter

---
+++ src/components/common/Feed/Buttons/index.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import getProducts from '../../../../store/products/selectors';
+
+type ButtonsType = {
+  setItem: any,
+  menuItems: any,
+};
+
+const Buttons = ({ setItem, menuItems }: ButtonsType) => {
+  const productData = useSelector(getProducts);
+  const [filter, setFilter]: [any, any] = useState('Все товары');
+
+  const filterItem = (curTag: string) => {
+    const newItem = productData.filter((newval) => newval.tag === curTag);
+    setItem(newItem);
+  };
+
+  return (
+    <div className="scrolling-wrapper">
+      <button
+        type="button"
+        className="menu-item--active"
+        onClick={() => setItem(productData)}>
+        Все товары
+      </button>
+      {menuItems.map((val: string) => (
+        <button
+          type="button"
+          id={val}
+          className={filter === { val } ? 'menu-item--active menu-item' : 'menu-item'}
+          onClick={() => filterItem(val)}>
+          {val}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default Buttons;
--- src/components/common/Feed/index.tsx
+++ src/components/common/Feed/index.tsx
@@ -4,32 +4,36 @@ import { useSelector } from 'react-redux';
 import style from './Feed.module.scss';
 import CustomButton from '../CustomButton';
 import LoadSvg from '../svg/LoadSvg';
-import MenuItem from '../MenuItem';
 import CardList from '../CardList';
 import getProducts from '../../../store/products/selectors';
+import Buttons from './Buttons';
 
 const Feed = () => {
   const productData = useSelector(getProducts);
+  const [item, setItem] = useState(productData);
 
-  const [filter, setFilter]: [any, any] = useState('Все товары');
-  const filterHandler = (event: any) => {
-    setFilter(event.target.getAttribute('id'));
-  };
+  const menuItems = ['Автомобили', 'Аксессуары', 'Мебель', 'Одежда', 'Спорт', 'Техника', 'Товары для дома'];
 
-  // @ts-ignore
-  const menuItems = [...new Set(productData.map((Val) => Val.tag))];
-  console.log(menuItems);
+  // const [filter, setFilter]: [any, any] = useState('Все товары');
+  // const filterHandler = (event: any) => {
+  //   setFilter(event.target.getAttribute('id'));
+  // };
+
   return (
     <div className={style.feedwrapper}>
       <nav className={style.scrollingWrapper}>
-        <MenuItem className={filter === 'Все товары' ? 'menu-item--active menu-item' : 'menu-item'} id="Все товары" onClick={filterHandler}>Все товары</MenuItem>
+        <Buttons
+          setItem={setItem}
+          menuItems={menuItems}
+          />
+        {/* <MenuItem className={filter === 'Все товары' ? 'menu-item--active menu-item' : 'menu-item'} id="Все товары" onClick={filterHandler}>Все товары</MenuItem>
         <MenuItem className={filter === 'Автомобили' ? 'menu-item--active  menu-item' : 'menu-item'} id="Автомобили" onClick={filterHandler}>Автомобили</MenuItem>
         <MenuItem className={filter === 'Аксессуары' ? 'menu-item--active  menu-item' : 'menu-item'} id="Аксессуары" onClick={filterHandler}>Аксессуары</MenuItem>
         <MenuItem className={filter === 'Мебель' ? 'menu-item--active  menu-item' : 'menu-item'} id="Мебель" onClick={filterHandler}>Мебель</MenuItem>
         <MenuItem className={filter === 'Одежда' ? 'menu-item--active  menu-item' : 'menu-item'} id="Одежда" onClick={filterHandler}>Одежда</MenuItem>
         <MenuItem className={filter === 'Спорт' ? 'menu-item--active  menu-item' : 'menu-item'} id="Спорт" onClick={filterHandler}>Спорт</MenuItem>
         <MenuItem className={filter === 'Техника' ? 'menu-item--active  menu-item' : 'menu-item'} id="Техника" onClick={filterHandler}>Техника</MenuItem>
-        <MenuItem className={filter === 'Товары для дома' ? 'menu-item--active  menu-item' : 'menu-item'} id="Товары для дома" onClick={filterHandler}>Товары для дома</MenuItem>
+        <MenuItem className={filter === 'Товары для дома' ? 'menu-item--active  menu-item' : 'menu-item'} id="Товары для дома" onClick={filterHandler}>Товары для дома</MenuItem> */}
       </nav>
       <div className={style.titleWrapper}>
         <h2 className={style.title}>Вся лента</h2>
@@ -37,7 +41,7 @@ const Feed = () => {
       <div className={style.cardsWrapper}>
         {/* TODO: add loader? */}
         {/* {productData ? <CardList data={productData} /> : <div className="lds-dual-ring" /> } */}
-        <CardList data={productData} />
+        <CardList data={item} />
       </div>
       <div className={style.btnWrapper}>
         <CustomButton onClick={() => {}} buttonStyle="btn--outline">
