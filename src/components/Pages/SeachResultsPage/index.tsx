9ab222f3167ce39e03400282c8daab8a13311162
Irina <irene7744@gmail.com>
2022-06-30 00:18:55 +0300

    feat: pagination search results

--- src/components/Pages/SeachResultsPage/index.tsx
+++ src/components/Pages/SeachResultsPage/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable consistent-return */
 /* eslint-disable array-callback-return */
 // import Search from 'antd/lib/input/Search';
-import { Pagination } from 'antd';
+import { List, Pagination } from 'antd';
 import moment from 'moment';
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
@@ -14,43 +14,47 @@ moment.locale('ru');
 
 const SeachResultsPage = () => {
   const search = useSelector(GetSearchTerm);
-  const data = useSelector(getProducts);
+  const productsData = useSelector(getProducts);
 
   const [posts, setPosts] = useState([]);
 
-  const results = data
-    .filter((val) => {
-      if (search === '') {
-        return val;
-      } if (val.title.toLowerCase().includes(search.toLowerCase())) {
-        return val;
-      }
-    })
-    .map((val) => (
-      <Link to={`/product/${val.id}`} key={val.id}>
-        <div className={style.productWrapper}>
-          <h2 className={style.title}>{val.title}</h2>
-          <p className={style.description}>{val.description}</p>
-          <div className={style.date}>
-            {moment(val.date).format('LL')}
-          </div>
-        </div>
-      </Link>
-    ));
+  const results = productsData.filter((val) => {
+    if (search === '') {
+      return val;
+    } if (val.title.toLowerCase().includes(search.toLowerCase())) {
+      return val;
+    }
+  });
+
+  const pageSize = 6;
+
   return (
     <div className="page-wrapper">
       <div className={style.pageWrapper}>
-        <span className={style.serchResulstNumber}>Найдено: </span>
-        {results.length}
+        <span className={style.serchResulstNumber}>
+          Найдено:
+          {' '}
+          {results.length}
+        </span>
 
-        {results}
-
+        <List
+          itemLayout="vertical"
+          size="large"
+          pagination={results.length > pageSize && { pageSize }}
+          dataSource={results}
+          renderItem={(data) => (
+            <Link to={`/product/${data.id}`} key={data.id}>
+              <div className={style.productWrapper}>
+                <h2 className={style.title}>{data.title}</h2>
+                <p className={style.description}>{data.description}</p>
+                <div className={style.date}>
+                  {moment(data.date).format('LL')}
+                </div>
+              </div>
+            </Link>
+          )}
+        />
       </div>
-      <Pagination
-        className={style.pagination}
-        defaultCurrent={1}
-        total={results.length}
-          />
     </div>
   );
 };
