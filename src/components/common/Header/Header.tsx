91e9e9f6d2ef03f320905c41da229a28bb6f067a
Irina <irene7744@gmail.com>
2022-06-30 15:20:41 +0300

    feat: add mobile menu

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable react/jsx-indent */
 import React, { useState } from 'react';
-import { MenuOutlined, SearchOutlined } from '@ant-design/icons';
+import { SearchOutlined } from '@ant-design/icons';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { Input } from 'antd';
@@ -13,11 +14,16 @@ import { GetUserName } from '../../../store/auth/selectors';
 import { SetSearchTermAction } from '../../../store/search/actions';
 import ShowSearchMobile from './ShowSearchMobile';
 import HideSearchMobile from './HideSearchMobile';
+import BurgerMenuIcon from './BurgerMenuIcon';
+import ProfileLockIcon from '../svg/ProfileLockIcon';
+import MobileMenu from './MobileMenu';
 
 const Header = () => {
   const name = useSelector(GetUserName);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
+  // close/open search on mobile
   const [open, setOpen] = useState(false);
   const showSearch = () => {
     setOpen(true);
@@ -26,14 +32,23 @@ const Header = () => {
     setOpen(false);
   };
 
-  const dispatch = useDispatch();
   const searchHandler = (searchTerm: string) => {
     dispatch(SetSearchTermAction(searchTerm));
     navigate('/search');
     setOpen(false);
   };
 
-  // close/open search on mobile
+  // Mobile Menu
+  const [openMenu, setOpenMenu] = useState(false);
+  const showMenu = () => {
+    setOpenMenu(true);
+    document.body.style.overflow = 'hidden';
+  };
+
+  const hideMenu = () => {
+    setOpenMenu(false);
+    document.body.style.overflow = '';
+  };
 
   return (
     <div className={style.wrapper}>
@@ -60,8 +75,7 @@ const Header = () => {
             {open && <Input.Search
               className={style.searchMobile}
               onSearch={searchHandler}
-              allowClear
-              /> }
+              allowClear />}
           </div>
         </div>
         <div className={style.button}>
@@ -72,12 +86,35 @@ const Header = () => {
               </CustomButton>
             </Link>) : <ModalBtn /> }
         </div>
-        <div className={style.profileLock} />
+        <div className={style.profileLockIcon}>
+          <ProfileLockIcon />
+        </div>
         <div className={style.profileButton}>
           {name ? <DropDownProfileBtn /> : <ProfileButton>Войти</ProfileButton> }
         </div>
         <div className={style.burger}>
-          <MenuOutlined style={{ fontSize: '24px', opacity: '0.4' }} />
+          {!openMenu
+            ? <BurgerMenuIcon onClick={showMenu} />
+            : <HideSearchMobile onClick={hideMenu} />}
+
+          {openMenu
+          && <div className={style.MobileMenu}>
+              <Link to="edit-ad">
+                <CustomButton onClick={() => {}} buttonStyle="btn--accent" type="button">
+                  Подать объявление
+                </CustomButton>
+              </Link>
+              <ul className={style.list}>
+                <li className={style.listItem}>Вся лента</li>
+                <li className={style.listItem}>Автомобили</li>
+                <li className={style.listItem}>Аксессуары</li>
+                <li className={style.listItem}>Мебель</li>
+                <li className={style.listItem}>Одежда</li>
+                <li className={style.listItem}>Спорт</li>
+                <li className={style.listItem}>Техника</li>
+                <li className={style.listItem}>Товары для дома</li>
+              </ul>
+             </div>}
         </div>
       </header>
     </div>
---
+++ src/components/common/Header/MobileMenu/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import CustomButton from '../../CustomButton';
+import style from './MobileMenu.module.scss';
+
+const MobileMenu = () => (
+  <h1>MOBILE MENU</h1>
+);
+
+export default MobileMenu;
